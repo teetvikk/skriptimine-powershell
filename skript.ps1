fdd8698451e6e17a58b057eaac9fa05d9f800cef
unknown <teet.russ@vikk.ee>
2026-01-15 06:00:11 +0200

    uus skript fail

---
+++ skript.ps1
@@ -0,0 +1,8 @@
+$nimi1 = "Teet"
+$nimi2 = "Russ"
+$kuupaev = Get-Date
+$lause = "PowerShelli skript t88tab"
+
+Write-Host "Skripti koostaja: $nimi1 $nimi2"
+Write-Host "Kuup2ev: $kuupaev"
+Write-Host "Lisainfo: $lause"
